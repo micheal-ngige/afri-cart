5d8644a179ba3e071c066fd824a2ec2b89c17d18
michael <michaelngige@gmail.com>
2024-04-22 08:48:12 +0300

    adding Products model

--- ecommerce/ecommerceapp/models.py
+++ ecommerce/ecommerceapp/models.py
@@ -24,17 +24,17 @@ class Product(models.Model):
         return self.product_name
     
 
-class Product(models.Model):
-    product_id = models.AutoField
-    product_name = models.CharField(max_length=100)
-    category = models.CharField(max_length=100, default="")
-    subcategory = models.CharField(max_length=50, default="")
-    price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=300)
-    image = models.ImageField(upload_to='images/images')
+# class Product(models.Model):
+#     product_id = models.AutoField
+#     product_name = models.CharField(max_length=100)
+#     category = models.CharField(max_length=100, default="")
+#     subcategory = models.CharField(max_length=50, default="")
+#     price = models.IntegerField(default=0)
+#     desc = models.CharField(max_length=300)
+#     image = models.ImageField(upload_to='images/images')
 
-    def __str__(self):
-        return self.product_name
+#     def __str__(self):
+#         return self.product_name
 
 
 
--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render,redirect
 from ecommerceapp.models import Contact,Product,OrderUpdate,Orders
 from django.contrib import messages
 from math import ceil
-
+# from ecommerceapp import keys
 from django.conf import settings
-
+# MERCHANT_KEY=keys.MK
 import json
 from django.views.decorators.csrf import  csrf_exempt
-
+# from PayTm import Checksum
 
 # Create your views here.
 def index(request):
@@ -68,13 +68,60 @@ def checkout(request):
         update = OrderUpdate(order_id=Order.order_id,update_desc="the order has been placed")
         update.save()
         # thank = True
-# # # mpesa PAYMENT INTEGRATION
+# # # PAYMENT INTEGRATION
 
-# 
+#         id = Order.order_id
+#         oid=str(id)+"ShopyCart"
+#         param_dict = {
+
+#             'MID':keys.MID,
+#             'ORDER_ID': oid,
+#             'TXN_AMOUNT': str(amount),
+#             'CUST_ID': email,
+#             'INDUSTRY_TYPE_ID': 'Retail',
+#             'WEBSITE': 'WEBSTAGING',
+#             'CHANNEL_ID': 'WEB',
+#             'CALLBACK_URL': 'http://127.0.0.1:8000/handlerequest/',
+
+#         }
+#         param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
+#         return render(request, 'paytm.html', {'param_dict': param_dict})
 
     return render(request, 'checkout.html')
 
 
+# @csrf_exempt
+# def handlerequest(request):
+#     # paytm will send you post request here
+#     form = request.POST
+#     response_dict = {}
+#     for i in form.keys():
+#         response_dict[i] = form[i]
+#         if i == 'CHECKSUMHASH':
+#             checksum = form[i]
+
+#     verify = Checksum.verify_checksum(response_dict, MERCHANT_KEY, checksum)
+#     if verify:
+#         if response_dict['RESPCODE'] == '01':
+#             print('order successful')
+#             a=response_dict['ORDERID']
+#             b=response_dict['TXNAMOUNT']
+#             rid=a.replace("ShopyCart","")
+           
+#             print(rid)
+#             filter2= Orders.objects.filter(order_id=rid)
+#             print(filter2)
+#             print(a,b)
+#             for post1 in filter2:
+
+#                 post1.oid=a
+#                 post1.amountpaid=b
+#                 post1.paymentstatus="PAID"
+#                 post1.save()
+#             print("run agede function")
+#         else:
+#             print('order was not successful because' + response_dict['RESPMSG'])
+#     return render(request, 'paymentstatus.html', {'response': response_dict})
 
 
 
